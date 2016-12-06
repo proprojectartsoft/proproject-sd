1e2beb3d06c562a0c5e2d345bd7d407283e86a03
proproject@artsoft-consult.ro <proproject@artsoft-consult.ro>
2016-12-06 14:57:09 +0200

    Revert "testing upload"
    This reverts commit fde0c9a7da032ebc3a4ab25444f1fc89ba37021e.

--- js/controllers/projects/AttachementsCtrl.js
+++ js/controllers/projects/AttachementsCtrl.js
@@ -1,57 +1,13 @@
 angular.module($APP.name).controller('AttachementsCtrl', AttachementsCtrl)
 
-AttachementsCtrl.$inject = ['$state', '$cordovaCamera', 'AttachmentsService'];
+AttachementsCtrl.$inject = ['$state'];
 
-function AttachementsCtrl($state, $cordovaCamera, AttachmentsService) {
+function AttachementsCtrl($state) {
     var vm = this;
-    vm.takePicture = takePicture;
     vm.go = go;
-
     vm.diaryId = localStorage.getObject('diaryId');
-    vm.filter = {};
 
-    AttachmentsService.get_attachment(vm.diaryId).then(function(result){
-      vm.pictures = result;
-      console.log(result);
-    })
-
-    vm.imgURI = [];
-vm.takePicture = function () {
-  var options = {
-    quality: 20,
-    destinationType: Camera.DestinationType.DATA_URL,
-    sourceType: Camera.PictureSourceType.CAMERA,
-    allowEdit: false,
-    encodingType: Camera.EncodingType.JPEG,
-    popoverOptions: CameraPopoverOptions,
-    saveToPhotoAlbum: true,
-    correctOrientation: true
-  };
-
-  $cordovaCamera.getPicture(options).then(function (imageData) {
-    $timeout(function () {
-      var alertPopup = $ionicPopup.alert({
-        title: 'Form gallery',
-        template: 'Photo added. Check form gallery for more options.'
-      });
-      vm.imgURI.push({
-        "id": 0,
-        "base64String": imageData,
-        "comment": "",
-        "tags": "",
-        "title": " ",
-        "projectId": 0,
-        "formInstanceId": 0
-      })
-      vm.filter.picture = vm.imgURI[vm.imgURI.length - 1];
-      vm.filter.state = 'form';
-      vm.filter.substate = null;
-    });
-  }, function (err) {
-    // An error occured. Show a message to the user
-  });
-};
-
+    vm.pictures = [];
 
     function go(predicate, id) {
       if((vm.diaryId) && (predicate ==='diary')){
