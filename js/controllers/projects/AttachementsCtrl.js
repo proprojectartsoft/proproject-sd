73b63f7b538d11ff13ad8900bead52dd85266de0
Proproject <proproject@artsoft-consult.ro>
2016-12-07 13:48:20 +0200

    Camera function

--- js/controllers/projects/AttachementsCtrl.js
+++ js/controllers/projects/AttachementsCtrl.js
@@ -1,13 +1,51 @@
 angular.module($APP.name).controller('AttachementsCtrl', AttachementsCtrl)
 
-AttachementsCtrl.$inject = ['$state'];
+AttachementsCtrl.$inject = ['$state','$cordovaCamera','$timeout'];
 
-function AttachementsCtrl($state) {
+function AttachementsCtrl($state,$cordovaCamera,$timeout) {
     var vm = this;
     vm.go = go;
+    vm.takePicture = takePicture;
     vm.diaryId = localStorage.getObject('diaryId');
 
     vm.pictures = [];
+    vm.filter = {};
+    vm.imgURI = [];
+    vm.takePicture = function () {
+      var options = {
+        quality: 20,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: false,
+        encodingType: Camera.EncodingType.JPEG,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: true,
+        correctOrientation: true
+      };
+
+      $cordovaCamera.getPicture(options).then(function (imageData) {
+        $timeout(function () {
+          // var alertPopup = $ionicPopup.alert({
+          //   title: 'Form gallery',
+          //   template: 'Photo added. Check form gallery for more options.'
+          //});
+          vm.imgURI.push({
+            "id": 0,
+            "base64String": imageData,
+            "comment": "",
+            "tags": "",
+            "title": " ",
+            "projectId": 0,
+            "formInstanceId": 0
+          })
+          vm.filter.picture = vm.imgURI[vm.imgURI.length - 1];
+          vm.filter.state = 'form';
+          vm.filter.substate = null;
+        });
+      }, function (err) {
+        // An error occured. Show a message to the user
+      });
+    };
 
     function go(predicate, id) {
       if((vm.diaryId) && (predicate ==='diary')){
