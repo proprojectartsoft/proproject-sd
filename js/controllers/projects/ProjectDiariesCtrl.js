1e547f91b00433aa8d80287e96f57b2fab9b820a
proproject@artsoft-consult.ro <proproject@artsoft-consult.ro>
2016-12-14 14:23:58 +0200

    contact -test

--- js/controllers/projects/ProjectDiariesCtrl.js
+++ js/controllers/projects/ProjectDiariesCtrl.js
@@ -14,7 +14,7 @@ function ProjectDiariesCtrl($scope, $timeout, $ionicModal, $ionicPopup, $state, 
     localStorage.setObject('editMode', null);
     SettingService.clearWeather();
     localStorage.setObject('diaryId', null);
-    localStorage.setObject('sd.attachments',null);
+    localStorage.setObject('sd.attachments', null);
     localStorage.setObject('projectId', $stateParams.id);
 
     vm.filter = {};
@@ -75,171 +75,116 @@ function ProjectDiariesCtrl($scope, $timeout, $ionicModal, $ionicPopup, $state, 
     })
 
     $scope.filter = {};
-    $scope.importContact = function (id) {
-            $timeout(function () {
-                navigator.contacts.pickContact(function (contact) {
-                    if (contact.emails) {
-                        $scope.filter.email = contact.emails[0].value;
-                        $timeout(function () {
-                            var alertPopupA = SecuredPopups.show('alert', {
-                                template: '<input type="email" ng-model="filter.email">',
-                                title: 'Share form',
-                                subTitle: 'Please insert an email address',
-                                scope: $scope,
-                                buttons: [
-                                    {text: '<i class="ion-person-add"></i>',
-                                        onTap: function (e) {
-                                            $scope.importContact(id);
-                                        }
-                                    },
-                                    {text: 'Cancel',
-                                        onTap: function (e) {
-                                            $ionicListDelegate.closeOptionButtons();
-                                        }
-                                    },
-                                    {
-                                        text: 'Send',
-                                        type: 'button-positive',
-                                        onTap: function (e) {
-                                            if ($scope.filter.email) {
-                                                var alertPopupB = SecuredPopups.show('alert', {
-                                                    title: "Sending email",
-                                                    template: "<center><ion-spinner icon='android'></ion-spinner></center>",
-                                                    content: "",
-                                                    buttons: []
-                                                });
-                                                ShareService.form.create(id, $scope.filter.email).then(function (response) {
-                                                    alertPopupB.close();
-                                                    if (response.message === "Form shared") {
-                                                        $scope.filter.email = "";
-                                                        var alertPopupC = SecuredPopups.show('alert', {
-                                                            title: 'Share',
-                                                            template: 'Email sent.'
-                                                        });
-                                                    } else {
-                                                        $scope.filter.email = "";
-                                                        var alertPopupC = SecuredPopups.show('alert', {
-                                                            title: 'Share',
-                                                            template: 'Form already shared to this user.'
-                                                        });
-                                                    }
-                                                });
-
-                                            } else {
-                                                var alertPopupC = SecuredPopups.show('alert', {
-                                                    title: 'Share',
-                                                    template: 'Please insert a valid value for email.'
-                                                });
-                                            }
-                                        }
-                                    }
-                                ]
-                            });
+    $scope.importContact = function(id) {
+        $timeout(function() {
+                navigator.contacts.pickContact(function(contact) {
+                        if (contact.emails) {
+                            $scope.filter.email = contact.emails[0].value;
+                            SharedService.share_diary(id, $scope.filter.email).then(function(result) {
+                                    console.log(result)
+                                }
+                            }
                         });
-                    }
-                }, function (err) {
                 });
-            });
         }
 
-    $scope.showPopup = function(predicate) {
-        $scope.data = {}
-        var myPopup = $ionicPopup.show({
-            template: '<input type = "email" ng-model = "data.model">',
-            title: 'Share site diary',
-            subTitle: 'Please insert an email address',
-            scope: $scope,
+        $scope.showPopup = function(predicate) {
+            $scope.data = {}
+            var myPopup = $ionicPopup.show({
+                template: '<input type = "email" ng-model = "data.model">',
+                title: 'Share site diary',
+                subTitle: 'Please insert an email address',
+                scope: $scope,
 
-            buttons: [{
-                text: 'Cancel'
-            }, {
-                text: '<b>Send</b>',
-                type: 'button-positive',
-                onTap: function(e) {
-                    if (!$scope.data.model) {
-                        e.preventDefault();
-                    } else {
-                        return $scope.data.model;
-                    }
-                }
-            }, {
-                text: '<i class="ion-person-add"></i>',
-                        onTap: function (e) {
-                            $scope.importContact(predicate.id);
+                buttons: [{
+                    text: 'Cancel'
+                }, {
+                    text: '<b>Send</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {
+                        if (!$scope.data.model) {
+                            e.preventDefault();
+                        } else {
+                            return $scope.data.model;
                         }
-            }, ]
-        });
-
-        myPopup.then(function(res) {
-            console.log('Tapped!', res);
-            if (res) {
-                var alertPopup1 = $ionicPopup.alert({
-                    title: "Sending email",
-                    template: "<center><ion-spinner icon='android'></ion-spinner></center>",
-                    content: "",
-                    buttons: []
-                });
-                alertPopup1.then(function(res) {
-                });
-                SharedService.share_diary(predicate.id, res).then(function(result) {
-                    console.log(result)
-                    alertPopup1.close();
-                    if (result.message === "Site diary Shared!") {
-                        res = "";
-                        var alertPopup = $ionicPopup.alert({
-                            title: 'Share',
-                            template: 'Email sent.'
-                        });
-                        alertPopup.then(function(res) {
-                            // Custom functionality....
-                        });
-                    } else {
-                        res = "";
-                        var alertPopup = $ionicPopup.alert({
-                            title: 'Share',
-                            template: 'Form already shared to this user.'
-                        });
-                        alertPopup.then(function(res) {
-                            // Custom functionality....
-                        });
                     }
+                }, {
+                    text: '<i class="ion-person-add"></i>',
+                    onTap: function(e) {
+                        $scope.importContact(predicate.id);
+                    }
+                }, ]
+            });
+
+            myPopup.then(function(res) {
+                console.log('Tapped!', res);
+                if (res) {
+                    var alertPopup1 = $ionicPopup.alert({
+                        title: "Sending email",
+                        template: "<center><ion-spinner icon='android'></ion-spinner></center>",
+                        content: "",
+                        buttons: []
+                    });
+                    alertPopup1.then(function(res) {});
+                    SharedService.share_diary(predicate.id, res).then(function(result) {
+                        console.log(result)
+                        alertPopup1.close();
+                        if (result.message === "Site diary Shared!") {
+                            res = "";
+                            var alertPopup = $ionicPopup.alert({
+                                title: 'Share',
+                                template: 'Email sent.'
+                            });
+                            alertPopup.then(function(res) {
+                                // Custom functionality....
+                            });
+                        } else {
+                            res = "";
+                            var alertPopup = $ionicPopup.alert({
+                                title: 'Share',
+                                template: 'Form already shared to this user.'
+                            });
+                            alertPopup.then(function(res) {
+                                // Custom functionality....
+                            });
+                        }
+                    })
+                }
+            });
+        };
+
+        function deleteDiary(id) {
+            SiteDiaryService.delete_diary(id).then(function(result) {
+                SiteDiaryService.list_diaries($stateParams.id).then(function(result) {
+                    vm.diaries = result;
                 })
-            }
-        });
-    };
-
-    function deleteDiary(id) {
-        SiteDiaryService.delete_diary(id).then(function(result) {
-            SiteDiaryService.list_diaries($stateParams.id).then(function(result) {
-                vm.diaries = result;
             })
-        })
-    }
+        }
 
-    function togglePlus() {
-        vm.show = !vm.show;
-    }
+        function togglePlus() {
+            vm.show = !vm.show;
+        }
 
-    function showDiary() {
-        vm.diaryModal.show();
-        vm.state = 'search';
-    }
+        function showDiary() {
+            vm.diaryModal.show();
+            vm.state = 'search';
+        }
 
-    function backDiary() {
-        vm.diaryModal.hide();
-        vm.show = false;
-    }
+        function backDiary() {
+            vm.diaryModal.hide();
+            vm.show = false;
+        }
 
-    function saveDiary() {
-        if (vm && vm.diaryModal) {
+        function saveDiary() {
+            if (vm && vm.diaryModal) {
+                vm.diaryModal.hide();
+            }
+        };
+
+        function go(predicate, id) {
+            $state.go('app.' + predicate, {
+                id: id
+            });
             vm.diaryModal.hide();
         }
-    };
-
-    function go(predicate, id) {
-        $state.go('app.' + predicate, {
-            id: id
-        });
-        vm.diaryModal.hide();
     }
-}
