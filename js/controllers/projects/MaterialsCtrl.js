c5cbb5bc92189277468a01b77a47ec17cde7bb0e
sebastianc_artsoft <sebastian.cocan@artsoft-consult.ro>
2017-05-19 14:51:52 +0300

    syntax errors

--- js/controllers/projects/MaterialsCtrl.js
+++ js/controllers/projects/MaterialsCtrl.js
@@ -57,6 +57,7 @@ function MaterialsCtrl($state, $scope, $ionicModal, $stateParams, SiteDiaryServi
             quantity: vm.create.plant_and_material_used[vm.index].quantity,
             tax: vm.create.plant_and_material_used[vm.index].tax,
             tax_formated: vm.create.plant_and_material_used[vm.index].tax && (vm.create.plant_and_material_used[vm.index].tax + " %") || '',
+            unitCost_formated: vm.create.plant_and_material_used[vm.index].cost_per_unit && (localStorage.getObject('currency') + " " + vm.create.plant_and_material_used[vm.index].cost_per_unit) || ''
         };
     }
 
