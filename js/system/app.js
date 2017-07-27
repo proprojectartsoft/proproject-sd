75981d26d8867c2ce50ec112ae50103743a2d7d3
ARTSOFT\proproject <proproject@artsoft-consult.ro>
2017-07-27 07:32:09 +0300

    PP-1508

--- js/system/app.js
+++ js/system/app.js
@@ -1,6 +1,7 @@
 var $APP = $APP || {};
 // $APP.server = 'http://app.preprod.proproject.io/';
 $APP.server = 'http://app.proproject.io/';
+// $APP.server = 'http://artvm23.vmnet.ro/';
 $APP.name = 'ppsd'
 
 angular.module($APP.name, [
