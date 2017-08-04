2a095d4828cd78f68b09360947a1df670d96a932
sebastianc_artsoft <sebastian.cocan@artsoft-consult.ro>
2017-08-04 14:23:15 +0300

    use indexedDB

--- js/controllers/projects/ProjectDiaryCtrl.js
+++ js/controllers/projects/ProjectDiaryCtrl.js
@@ -29,6 +29,22 @@ function ProjectDiaryCtrl($rootScope, $ionicPopup, $timeout, $state, $stateParam
             var offDiary = localStorage.getObject('diaryToSync');
             //TODO:
 
+            $indexedDB.openStore('projects', function(store) {
+                vm.projectId = parseInt(vm.projectId);
+                store.find(vm.projectId).then(function(e) {
+                    vm.diaries = e.value.diaries;
+                    angular.forEach(vm.diaries, function(diary) {
+                        if (diary.id == $stateParams.id) {
+                            vm.created_for_date = (diary.created_for_date != 0) && diary.created_for_date || '';
+                            localStorage.setObject('sd.diary.create', diary.data);
+                            vm.summary = diary.data ? diary.data.summary : '';
+                        }
+                    })
+                });
+            });
+
+
+
             vm.create = offDiary.data;
             vm.created_for_date = (vm.create.created_for_date != 0) && vm.create.created_for_date || '';
             vm.summary = vm.create.summary;
@@ -148,20 +164,21 @@ function ProjectDiaryCtrl($rootScope, $ionicPopup, $timeout, $state, $stateParam
                 if (attStorage) {
                     diary.attachments = attStorage;
                 }
-                diariesToSync.push(diary);
-                localStorage.setObject('diariesToSync', diariesToSync);
+                diariesToSync.push(diary); //NOT
+                localStorage.setObject('diariesToSync', diariesToSync); //NOT
 
 
+//TODO:
 
+                var proj = localStorage.getObject('currentProj');
+                if (!proj.value.diaries)
+                    proj.value.diaries = [];
+                diary.id = "off" + proj.value.diaries.length + 1;
+                proj.value.diaries.push(diary);
+                localStorage.setObject('currentProj', proj);
+                saveChanges(localStorage.getObject('currentProj'));
+                localStorage.setObject('initialProj', localStorage.getObject('currentProj'));
 
-                // vm.diaryToSync = {
-                //     data: vm.create
-                // };
-                // if (attStorage) {
-                //     vm.diaryToSync.attachments = attStorage;
-                // }
-                // localStorage.setObject('diaryToSync', vm.diaryToSync);
-                //
 
 
 
@@ -193,7 +210,6 @@ function ProjectDiaryCtrl($rootScope, $ionicPopup, $timeout, $state, $stateParam
         });
         // if (navigator.onLine && localStorage.getObject('diaryToSync')) {
         if (navigator.onLine && localStorage.getObject('diariesToSync')) {
-
             SyncService.addDiariesToSync().then(function() {
                 addSiteDiaryToDB(syncPopup)
             })
@@ -202,234 +218,234 @@ function ProjectDiaryCtrl($rootScope, $ionicPopup, $timeout, $state, $stateParam
         }
     }
 
-    function saveEdit() {
-        if (!navigator.onLine) {
-            var syncPopup = $ionicPopup.show({
-                title: 'You are offline',
-                template: "<center>You can edit Site Diaries while online.</center>",
-                content: "",
-                buttons: [{
-                    text: 'OK',
-                    type: 'button-positive',
-                    onTap: function(e) {
-                        syncPopup.close();
-                    }
-                }]
-            });
-            return;
-        }
+    function saveEdit()
+    if (!navigator.onLine) {
         var syncPopup = $ionicPopup.show({
-            title: 'Submitting',
-            template: "<center><ion-spinner icon='android'></ion-spinner></center>",
+            title: 'You are offline',
+            template: "<center>You can edit Site Diaries while online.</center>",
             content: "",
-            buttons: []
-        });
-        $('.save-btn').attr("disabled", true);
-        vm.edit = false;
-        localStorage.setObject('editMode', vm.edit);
-        vm.create = localStorage.getObject('sd.diary.create');
-        var updateDiary = SiteDiaryService.update_diary(vm.create).success(function(result) {
-            vm.go('project');
-        }).error(function(err) {
-            var errPopup = $ionicPopup.show({
-                title: "Error",
-                template: '<span>An unexpected error occured and Site Diary could not be updated.</span>',
-                buttons: [{
-                    text: 'OK',
-                    type: 'button-positive',
-                    onTap: function(e) {
-                        errPopup.close();
-                    }
-                }]
-            });
-        })
-        angular.forEach(localStorage.getObject('sd.comments'), function(comment) {
-            SiteDiaryService.add_comments(comment)
-                .success(function(result) {
-                    localStorage.setObject('sd.comments', []);
-                }).error(function(err) {
-                    localStorage.setObject('sd.comments', []);
-                })
-        })
-        var attachments = localStorage.getObject('sd.attachments');
-        var attToAdd = [];
-        if (attachments !== null) {
-            angular.forEach(attachments.pictures, function(value) {
-                if (!value.path) {
-                    attToAdd.push(value);
+            buttons: [{
+                text: 'OK',
+                type: 'button-positive',
+                onTap: function(e) {
+                    syncPopup.close();
                 }
-            });
-            var uploadAttachments = AttachmentsService.upload_attachments(attToAdd).then(function(result) {
+            }]
+        });
+        return;
+    }
+    var syncPopup = $ionicPopup.show({
+        title: 'Submitting',
+        template: "<center><ion-spinner icon='android'></ion-spinner></center>",
+        content: "",
+        buttons: []
+    });
+    $('.save-btn').attr("disabled", true);
+    vm.edit = false;
+    localStorage.setObject('editMode', vm.edit);
+    vm.create = localStorage.getObject('sd.diary.create');
+    var updateDiary = SiteDiaryService.update_diary(vm.create).success(function(result) {
+        vm.go('project');
+    }).error(function(err) {
+        var errPopup = $ionicPopup.show({
+            title: "Error",
+            template: '<span>An unexpected error occured and Site Diary could not be updated.</span>',
+            buttons: [{
+                text: 'OK',
+                type: 'button-positive',
+                onTap: function(e) {
+                    errPopup.close();
+                }
+            }]
+        });
+    })
+    angular.forEach(localStorage.getObject('sd.comments'), function(comment) {
+        SiteDiaryService.add_comments(comment)
+            .success(function(result) {
+                localStorage.setObject('sd.comments', []);
+            }).error(function(err) {
+                localStorage.setObject('sd.comments', []);
+            })
+    })
+    var attachments = localStorage.getObject('sd.attachments');
+    var attToAdd = [];
+    if (attachments !== null) {
+        angular.forEach(attachments.pictures, function(value) {
+            if (!value.path) {
+                attToAdd.push(value);
+            }
+        });
+        var uploadAttachments = AttachmentsService.upload_attachments(attToAdd).then(function(result) {
+            console.log(result);
+        });
+
+        var updateAttachments = [];
+        if (attachments.toBeUpdated && attachments.toBeUpdated.length != 0) {
+            angular.forEach(attachments.toBeUpdated, function(att) {
+                updateAttachments.push(AttachmentsService.update_attachments(att).then(function(result) {
+                    console.log(result);
+                }));
+            })
+        }
+
+        var deleteAttachments;
+        if (attachments.toBeDeleted) {
+            deleteAttachments = AttachmentsService.delete_attachments(attachments.toBeDeleted).then(function(result) {
                 console.log(result);
             });
-
-            var updateAttachments = [];
-            if (attachments.toBeUpdated && attachments.toBeUpdated.length != 0) {
-                angular.forEach(attachments.toBeUpdated, function(att) {
-                    updateAttachments.push(AttachmentsService.update_attachments(att).then(function(result) {
-                        console.log(result);
-                    }));
-                })
-            }
-
-            var deleteAttachments;
-            if (attachments.toBeDeleted) {
-                deleteAttachments = AttachmentsService.delete_attachments(attachments.toBeDeleted).then(function(result) {
-                    console.log(result);
-                });
-            }
         }
+    }
+    var proj = localStorage.getObject('currentProj');
+    var diary = $filter('filter')(proj.value.diaries, {
+        id: (vm.diaryId)
+    })[0];
+    diary.created_for_date = vm.create.created_for_date;
+    diary.data.summary = vm.create.summary;
+    localStorage.setObject('currentProj', proj);
+    saveChanges(localStorage.getObject('currentProj'));
+    localStorage.setObject('initialProj', localStorage.getObject('currentProj'));
+    $('.save-btn').attr("disabled", false);
+
+    Promise.all([updateDiary, uploadAttachments, updateAttachments, deleteAttachments]).then(syncPopup.close);
+}
+
+function setCreatedDateFor() {
+    var create = localStorage.getObject('sd.diary.create');
+    create.created_for_date = new Date(vm.created_for_date).getTime();
+    localStorage.setObject('sd.diary.create', create);
+}
+
+function setSummary() {
+    var summaryPopup = $ionicPopup.show({
+        title: "Site Diary Summary",
+        template: '<textarea rows="5" class="summary-textarea" ng-model="vm.summary" placeholder="Please add any summary of the days work you have here."></textarea>',
+        content: "",
+        cssClass: 'summary-popup',
+        scope: $scope,
+        buttons: [{
+            text: 'Save',
+            type: 'button-positive',
+            onTap: function(e) {
+                var create = localStorage.getObject('sd.diary.create');
+                create.summary = vm.summary;
+                localStorage.setObject('sd.diary.create', create);
+                if (!vm.edit && !vm.enableCreate) {
+                    saveSummary(create);
+                }
+                summaryPopup.close();
+            }
+        }, {
+            text: 'Cancel',
+        }]
+    });
+}
+
+function saveSummary(create) {
+    if (!navigator.onLine) {
+        var syncPopup = $ionicPopup.show({
+            title: 'You are offline',
+            template: "<center>You can edit Site Diaries while online.</center>",
+            content: "",
+            buttons: [{
+                text: 'OK',
+                type: 'button-positive',
+                onTap: function(e) {
+                    syncPopup.close();
+                }
+            }]
+        });
+        return;
+    }
+    var syncPopup = $ionicPopup.show({
+        title: 'Submitting',
+        template: "<center><ion-spinner icon='android'></ion-spinner></center>",
+        content: "",
+        buttons: []
+    });
+
+    SiteDiaryService.update_diary(create).success(function(result) {
+        // vm.go('project');
         var proj = localStorage.getObject('currentProj');
         var diary = $filter('filter')(proj.value.diaries, {
             id: (vm.diaryId)
         })[0];
-        diary.created_for_date = vm.create.created_for_date;
-        diary.data.summary = vm.create.summary;
+        diary.created_for_date = create.created_for_date;
+        diary.data.summary = create.summary;
         localStorage.setObject('currentProj', proj);
         saveChanges(localStorage.getObject('currentProj'));
         localStorage.setObject('initialProj', localStorage.getObject('currentProj'));
-        $('.save-btn').attr("disabled", false);
-
-        Promise.all([updateDiary, uploadAttachments, updateAttachments, deleteAttachments]).then(syncPopup.close);
-    }
-
-    function setCreatedDateFor() {
-        var create = localStorage.getObject('sd.diary.create');
-        create.created_for_date = new Date(vm.created_for_date).getTime();
-        localStorage.setObject('sd.diary.create', create);
-    }
-
-    function setSummary() {
-        var summaryPopup = $ionicPopup.show({
-            title: "Site Diary Summary",
-            template: '<textarea rows="5" class="summary-textarea" ng-model="vm.summary" placeholder="Please add any summary of the days work you have here."></textarea>',
-            content: "",
-            cssClass: 'summary-popup',
-            scope: $scope,
+        syncPopup.close();
+    }).error(function(err) {
+        var errPopup = $ionicPopup.show({
+            title: "Error",
+            template: '<span>An unexpected error occured and Site Diary could not be updated.</span>',
             buttons: [{
-                text: 'Save',
+                text: 'OK',
                 type: 'button-positive',
                 onTap: function(e) {
-                    var create = localStorage.getObject('sd.diary.create');
-                    create.summary = vm.summary;
-                    localStorage.setObject('sd.diary.create', create);
-                    if (!vm.edit && !vm.enableCreate) {
-                        saveSummary(create);
-                    }
-                    summaryPopup.close();
+                    errPopup.close();
                 }
-            }, {
-                text: 'Cancel',
             }]
         });
-    }
+    })
+}
 
-    function saveSummary(create) {
-        if (!navigator.onLine) {
-            var syncPopup = $ionicPopup.show({
-                title: 'You are offline',
-                template: "<center>You can edit Site Diaries while online.</center>",
-                content: "",
-                buttons: [{
-                    text: 'OK',
-                    type: 'button-positive',
-                    onTap: function(e) {
-                        syncPopup.close();
-                    }
-                }]
-            });
-            return;
-        }
+function toggle() {
+    if (!navigator.onLine) {
         var syncPopup = $ionicPopup.show({
-            title: 'Submitting',
-            template: "<center><ion-spinner icon='android'></ion-spinner></center>",
+            title: 'You are offline',
+            template: "<center>You can edit Site Diaries while online.</center>",
             content: "",
-            buttons: []
-        });
-
-        SiteDiaryService.update_diary(create).success(function(result) {
-            // vm.go('project');
-            var proj = localStorage.getObject('currentProj');
-            var diary = $filter('filter')(proj.value.diaries, {
-                id: (vm.diaryId)
-            })[0];
-            diary.created_for_date = create.created_for_date;
-            diary.data.summary = create.summary;
-            localStorage.setObject('currentProj', proj);
-            saveChanges(localStorage.getObject('currentProj'));
-            localStorage.setObject('initialProj', localStorage.getObject('currentProj'));
-            syncPopup.close();
-        }).error(function(err) {
-            var errPopup = $ionicPopup.show({
-                title: "Error",
-                template: '<span>An unexpected error occured and Site Diary could not be updated.</span>',
-                buttons: [{
-                    text: 'OK',
-                    type: 'button-positive',
-                    onTap: function(e) {
-                        errPopup.close();
-                    }
-                }]
-            });
-        })
-    }
-
-    function toggle() {
-        if (!navigator.onLine) {
-            var syncPopup = $ionicPopup.show({
-                title: 'You are offline',
-                template: "<center>You can edit Site Diaries while online.</center>",
-                content: "",
-                buttons: [{
-                    text: 'OK',
-                    type: 'button-positive',
-                    onTap: function(e) {
-                        syncPopup.close();
-                    }
-                }]
-            });
-            return;
-        }
-        vm.edit = !vm.edit;
-        localStorage.setObject('editMode', vm.edit);
-        if (!vm.edit) {
-            localStorage.setObject('currentProj', localStorage.getObject('initialProj'));
-        }
-    }
-
-    function go(predicate, id) {
-        if (predicate === 'project') {
-            localStorage.setObject('currentProj', localStorage.getObject('initialProj'));
-            $state.go('app.' + predicate, {
-                id: vm.projectId
-            });
-        } else {
-            $state.go('app.' + predicate, {
-                id: id
-            });
-        }
-    }
-
-    function saveChanges(project) {
-        localStorage.setObject('sd.seen', {});
-        $indexedDB.openStore('projects', function(store) {
-            store.upsert(project).then(
-                function(e) {},
-                function(err) {
-                    var offlinePopup = $ionicPopup.alert({
-                        title: "Unexpected error",
-                        template: "<center>An unexpected error occurred while trying to update Site Diary.</center>",
-                        content: "",
-                        buttons: [{
-                            text: 'Ok',
-                            type: 'button-positive',
-                            onTap: function(e) {
-                                offlinePopup.close();
-                            }
-                        }]
-                    });
+            buttons: [{
+                text: 'OK',
+                type: 'button-positive',
+                onTap: function(e) {
+                    syncPopup.close();
                 }
-            )
-        })
+            }]
+        });
+        return;
     }
+    vm.edit = !vm.edit;
+    localStorage.setObject('editMode', vm.edit);
+    if (!vm.edit) {
+        localStorage.setObject('currentProj', localStorage.getObject('initialProj'));
+    }
+}
+
+function go(predicate, id) {
+    if (predicate === 'project') {
+        localStorage.setObject('currentProj', localStorage.getObject('initialProj'));
+        $state.go('app.' + predicate, {
+            id: vm.projectId
+        });
+    } else {
+        $state.go('app.' + predicate, {
+            id: id
+        });
+    }
+}
+
+function saveChanges(project) {
+    localStorage.setObject('sd.seen', {});
+    $indexedDB.openStore('projects', function(store) {
+        store.upsert(project).then(
+            function(e) {},
+            function(err) {
+                var offlinePopup = $ionicPopup.alert({
+                    title: "Unexpected error",
+                    template: "<center>An unexpected error occurred while trying to update Site Diary.</center>",
+                    content: "",
+                    buttons: [{
+                        text: 'Ok',
+                        type: 'button-positive',
+                        onTap: function(e) {
+                            offlinePopup.close();
+                        }
+                    }]
+                });
+            }
+        )
+    })
+}
 }
