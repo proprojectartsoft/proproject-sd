959569c0885820af3f3640753ae4148c6b391253
sebastianc_artsoft <sebastian.cocan@artsoft-consult.ro>
2017-08-04 17:22:53 +0300

    use foreach

--- js/services/SyncService.js
+++ js/services/SyncService.js
@@ -238,13 +238,18 @@ angular.module($APP.name).factory('SyncService', [
                                 $indexedDB.openStore('projects', function(store) {
                                     store.getAll().then(function(result) {
                                         angular.forEach(result, function(project) {
-                                            console.log(diariesToAdd);
-                                            var copy = angular.copy(diariesToAdd);
-                                            console.log(copy);
-                                            angular.extend(diariesToAdd, copy, $filter('filter')(project.value.diaries, function(d) {
+                                            angular.forEach($filter('filter')(project.value.diaries, function(d) {
                                                 return /^off.*/g.test(d.id);
-                                            }))
-                                            console.log(diariesToAdd);
+                                            }), function(sd) {
+                                                diariesToAdd.push(sd);
+                                            })
+                                            // console.log(diariesToAdd);
+                                            // var copy = angular.copy(diariesToAdd);
+                                            // console.log(copy);
+                                            // angular.extend(diariesToAdd, copy, $filter('filter')(project.value.diaries, function(d) {
+                                            //     return /^off.*/g.test(d.id);
+                                            // }))
+                                            // console.log(diariesToAdd);
                                         })
                                         localStorage.removeItem('diariesToSync');
                                         if (diariesToAdd && !diariesToAdd.length) {
