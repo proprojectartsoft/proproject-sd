beba5b369d677225e4d90e664162e9d125a1f471
Laszlo Krizsan <laszlo.krizsan@artsoft-consult.ro>
2017-09-05 18:40:01 +0300

    #noissue - frontend server added

---
+++ !!!browser.bat
@@ -0,0 +1,1 @@
+start chrome --user-data-dir="C:\Chrome dev session" --disable-web-security "http://localhost:8000/index.html"
---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+
+var app = express();
+// Port for the server
+var port = process.env.PORT || 8000;
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, './')));
+
+app.get('*', function (req, res) {
+    res.redirect('/');
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
+
+// don't crash server on undefined errors
+process.on('uncaughtException', function (err) {
+    console.log('Undefined server error: ' + err);
+});
+
+// Start Server on specified port
+app.listen(port, function () {
+    console.log("Express server listening on port " + port);
+});
