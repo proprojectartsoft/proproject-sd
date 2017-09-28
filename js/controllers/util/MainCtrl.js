0ab3bdc54471375dbda8e6c8e9080971ef1a7a34
Laszlo Krizsan <laszlo.krizsan@artsoft-consult.ro>
2017-09-28 21:46:57 +0300

    refactor - .go refactored

--- js/controllers/util/MainCtrl.js
+++ js/controllers/util/MainCtrl.js
@@ -5,7 +5,9 @@ sdApp.controller('MainCtrl', [
 	'$state',
 	'$stateParams',
 	'$ionicSideMenuDelegate',
-	function ($rootScope, $timeout, $scope, $state, $stateParams, $ionicSideMenuDelegate) {
+	'$ionicHistory',
+	'$window',
+	function ($rootScope, $timeout, $scope, $state, $stateParams, $ionicSideMenuDelegate, $ionicHistory, $window) {
 		/**
 		 * Method to go somewhere
 		 * @param {String} where - app state to go to
@@ -14,32 +16,34 @@ sdApp.controller('MainCtrl', [
 		 */
 		$rootScope.go = function (where, params, reload) {
 			//if ($ionicSideMenuDelegate.isOpen()) $ionicSideMenuDelegate.toggleLeft();
-			
-			// this would be the forced reload of a profile page
-			if (where === 'reload') {
-				return $state.reload();
-			}
-			
-			if (!reload) reload = false;
-			if (!params) params = $stateParams;
-			
-			if (where === $state.current.name) {
-				// This could bite us later, need it to reload the URLs with parameters included
-				// params = {}
-				reload = true;
-			}
-			
-			var reloadParam = {
-				'reload': reload
-			};
-			
-			if (reload) {
-				reloadParam.location = 'replace';
-				reloadParam.inherit = false;
-				reloadParam.notify = true;
-			}
-			
-			$state.transitionTo(where, params, reloadParam);
+			$ionicHistory.clearCache().then(function () {
+				
+				// this would be the forced reload of a profile page
+				if (where === 'reload') {
+					return $state.reload();
+				}
+				
+				if (!reload) reload = false;
+				if (!params) params = $stateParams;
+				
+				if (where === $state.current.name) {
+					//console.log('Reloading');
+					return $window.location.reload();
+				}
+				
+				var reloadParam = {
+					reload: reload
+				};
+				
+				if (reload) {
+					reloadParam.location = 'replace';
+					reloadParam.inherit = false;
+					reloadParam.notify = true;
+				}
+				
+				//console.log('We are loading the page', where, params, reloadParam);
+				$state.go(where, params, reloadParam);
+			});
 		};
 		
 	}
